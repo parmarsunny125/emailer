66670b9a413e7b170b966c624313a786b545972a
parmarsunny125 <87124328+parmarsunny125@users.noreply.github.com>
2022-12-28 00:30:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  {disableReactDevtools} from '@fvilers/disable-react-devtools'
-if(process.env.NODE_ENV==='production') disableReactDevtools()
+import  {disableReactDevTools} from '@fvilers/disable-react-devtools'
+if(process.env.NODE_ENV==='production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

66ccb1c186e189b34805aadac56b65235ec2918c
chandnijiv2001 <parmarsunny125@gmail.com>
2022-12-28 00:04:59 +0530

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import  {disableReactDevtools} from '@fvilers/disable-react-devtools'
+if(process.env.NODE_ENV==='production') disableReactDevtools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
